8a2d7343d4fbcc748dd2531f6e896f4593bee68b
verseler <handuman.verselerkerr@gmail.com>
2024-05-23 10:43:08 +0800

    add comment

--- models/Assignment.model.js
+++ models/Assignment.model.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
+// Define the schema for assignments
 const Schema = mongoose.Schema;
 
 const AssignmentSchema = new Schema({
+    // the name is string and it is required
     name: {
         require: true,
         type: String
     },
+     // the dueDate is string and it is required
     dueDate: {
         require: true,
         type: String
